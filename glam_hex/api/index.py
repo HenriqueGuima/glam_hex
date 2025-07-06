84d3f74ed709569631db09c01ff044b51733cd81
Henrique Guima <hbertoguima@gmail.com>
2025-07-06 21:42:26 +0100

    redeploy

--- glam_hex/api/index.py
+++ glam_hex/api/index.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'glam_hex'))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'glam_hex.settings')
+
 from glam_hex.wsgi import application
 
 app = application
